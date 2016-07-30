3b5155458730b54b979977502ef048b609d075eb
ligx <604353886@qq.com>
2016-07-30 13:13:40 +0800

    add script

---
+++ AppManager.py
@@ -0,0 +1,49 @@
+#coding=utf-8
+__author__ = 'lgx'
+
+import sys
+import subprocess
+
+if len(sys.argv) < 2:
+    print("Usage: python AppManager.py app start|stop|status|restart")
+
+app = sys.argv[0]
+action = sys.argv[1]
+
+
+def start():
+    subprocess.call("./app_start.sh", shell=True)
+
+def stop():
+    pid = find_app_process()
+    if pid > 0:
+        subprocess.call("kill -9 %s" % str(pid), shell=True)
+
+def status():
+    pid = find_app_process()
+    if pid > 0:
+        print("%s is running" % app)
+    else:
+        print("%s is not running" % app)
+
+def restart():
+    stop()
+    start()
+
+
+def find_app_process():
+    shell = "ps -ef | grep %s | grep java | awk 'print $2'" % sys.argv[1]
+    child = subprocess.Popen(shell, shell=True, stdout=subprocess.PIPE)
+    pid = child.communicate()
+    return pid
+
+
+if action == "start":
+    start()
+elif action == "stop":
+    stop()
+elif action == "status":
+    status()
+elif action == "restart":
+    restart()
+
