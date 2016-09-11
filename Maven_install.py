5a3d0192f5e494853b572dffe4e7698cd70d5674
ligx <604353886@qq.com>
2016-09-11 23:39:36 +0800

    python script

--- Maven_install.py
+++ Maven_install.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 import subprocess
 import os
 import sys
@@ -8,36 +8,45 @@ __author__ = 'lgx'
 mvn_install_shell = 'mvn clean install -Dmaven.test.skip=true'
 mvn_deploy_shell = 'mvn clean deploy -Dmaven.test.skip=true'
 
+
 def install_or_deploy_commons(shell):
     os.chdir('/Users/ligx/enjoy_project/commons')
     subprocess.call(shell, shell=True)
+
 
 def install_or_deploy_base(shell):
     os.chdir('/Users/ligx/enjoy_project/base')
     subprocess.call(shell, shell=True)
 
+
 def install_or_deploy_pay(shell):
     os.chdir('/Users/ligx/enjoy_project/pay/')
     subprocess.call(shell, shell=True)
+
 
 def install_or_deploy_order(shell):
     os.chdir('/Users/ligx/enjoy_project/order/')
     subprocess.call(shell, shell=True)
 
+
 def install_or_deploy_aggregator(shell):
     os.chdir('/Users/ligx/enjoy_project/aggregator/')
     subprocess.call(shell, shell=True)
+
 
 def install_or_deploy_3_project(shell):
     install_or_deploy_pay(shell)
     install_or_deploy_order(shell)
     install_or_deploy_aggregator(shell)
 
+
 def install_or_deploy_4_project(shell):
     install_or_deploy_base(shell)
     install_or_deploy_pay(shell)
     install_or_deploy_order(shell)
     install_or_deploy_aggregator(shell)
+
+
 def install_or_deploy_5_project(shell):
     install_or_deploy_commons(shell)
     install_or_deploy_base(shell)
